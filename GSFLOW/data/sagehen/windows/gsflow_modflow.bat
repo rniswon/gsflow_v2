32dd1a7d28e74e585ea2c7e84b192a4c21344b16
Steve Regan <rsregan@usgs.gov>
2018-12-17 20:05:49 -0700

    adding lots of output_test files and updated PRMS input files and control and bat files

--- GSFLOW/data/sagehen/windows/gsflow_modflow.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-..\..\..\bin\gsflow .\gsflow_modflow.control
-ECHO.
-ECHO Run complete. Please press enter when you want to continue.
-PAUSE>NUL
---
+++ GSFLOW/data/sagehen/windows/prms_map_results.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+..\..\..\bin\gsflow .\prms_map_results.control
+ECHO.
+ECHO Run complete. Please press enter when you want to continue.
+PAUSE>NUL
