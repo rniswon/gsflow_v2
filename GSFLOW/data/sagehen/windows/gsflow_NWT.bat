3b8ffa9a31d1d1b15e1ab311852552da4c41bf51
Steve Regan <rsregan@usgs.gov>
2020-01-14 13:57:56 -0700

    reran examples using soilzone_aet_flag = 1

--- GSFLOW/data/sagehen/windows/gsflow_NWT.bat
+++ GSFLOW/data/sagehen/windows/gsflow_NWT.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-..\..\..\bin\gsflow .\gsflow.control -set modflow_name ..\input\modflow\sagehen_NWT.nam -set subbasin_flag 0 -set print_debug -2 -set gsflow_output_file ..\output\gsflow_nwt.out
+..\..\..\bin\gsflow .\gsflow.control -set modflow_name ..\input\modflow\sagehen_NWT.nam -set subbasin_flag 0 -set print_debug -2 -set gsflow_output_file ..\output\gsflow_nwt.out -set soilzone_aet_flag 1
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
