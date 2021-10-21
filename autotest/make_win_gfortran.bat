97602cad31d7b236531162a2243655127d022cef
rniswon <rniswon@usgs.gov>
2021-10-21 16:30:02 -0700

    udpated make_win_gfortan to use new python environment

--- autotest/make_win_gfortran.bat
+++ autotest/make_win_gfortran.bat
@@ -1,6 +1,6 @@
 set PATH=%PATH%;C:\Program Files (x86)\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin
 gfortran --version
 gcc --version
-:: C:\Users\rniswon\AppData\Local\Continuum\miniconda3\envs\py39\python.exe make_gfortran.py -fc gfortran -cc gcc -sd -mc -e ..\GSFLOW\src gsflow.exe
+C:\Users\rniswon\Anaconda3\envs\py39\python.exe make_gfortran.py -fc gfortran -cc gcc -sd -mc -e ..\GSFLOW\src gsflow.exe
 make_gfortran.py -fc gfortran -cc gcc -sd -mc ..\GSFLOW\src gsflow.exe
 pause
