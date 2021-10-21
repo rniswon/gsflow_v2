e99ddda209eb3cac86280c4d4be168492be9b5e7
rniswon <rniswon@usgs.gov>
2021-10-21 10:59:48 -0700

    updated output results for ag2a; changed environment used for compiling for autotest

--- autotest/make_gfortran.py
+++ autotest/make_gfortran.py
@@ -37,9 +37,9 @@ def cleanup(srcdir, tempdir):
     try:
         if platform.system().lower() == "windows":
             os.remove(os.path.join('stemp', 'prms', 'utils_prms_linux.f90'))
-            os.remove(os.path.join('stemp', 'prms', 'utils_prms.f90'))
+ #           os.remove(os.path.join('stemp', 'prms', 'utils_prms.f90'))
         else:
-            os.remove(os.path.join('stemp', 'prms', 'utils_prms.f90'))
+ #           os.remove(os.path.join('stemp', 'prms', 'utils_prms.f90'))
             os.remove(os.path.join('stemp', 'prms', 'utils_prms_windows.f90'))
     except:
         pass
