33c1c712a72f7add385462f7301f1607b7e660a1
rniswon <rniswon@usgs.gov>
2021-12-17 08:16:29 -0800

    comment out if statement in sm_read_parameter_file.f90 to get models to run

--- GSFLOW/src/prms/sm_read_parameter_file.f90
+++ GSFLOW/src/prms/sm_read_parameter_file.f90
@@ -598,7 +598,7 @@ contains
 
     if (Parameter_check_flag == 1) then
       do i = 1, Numvalues
-        if ( Hru_type(i)==0 .OR. Hru_type(i)==2 ) CYCLE
+        !if ( Hru_type(i)==0 .OR. Hru_type(i)==2 ) CYCLE
         if ( Parameter_data(param_id)%values_real_1d(i) > Parameter_data(param_id)%maximum ) then
           print '(/,3A,I0,A,I0)', 'WARNING, value > maximum value for parameter: ', Paramname, '; index: ', param_id, '; HRU: ', i
           print '(A,F0.5,A,F0.5)', '         value: ', Parameter_data(param_id)%values_real_1d(i), '; maximum value: ', &
