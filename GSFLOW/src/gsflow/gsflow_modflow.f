85637346e5ffad6fb634a6e67df8e283b0b36e5d
rniswon <rniswon@usgs.gov>
2022-01-18 15:35:56 -0800

    fixed double RHS in SFRFM double loop; added call to AG2MODSIM routine in gsflow_modflow

--- GSFLOW/src/gsflow/gsflow_modflow.f
+++ GSFLOW/src/gsflow/gsflow_modflow.f
@@ -642,7 +642,7 @@ C7C2A---FORMULATE THE FINITE DIFFERENCE EQUATIONS.
 !     1                              IUNIT(44),IUNIT(52),IUNIT(55),IGRID)
 
 !  Call the PRMS modules that need to be inside the iteration loop
-            IF ( Szcheck==ACTIVE ) THEN
+            IF ( Szcheck==ACTIVE .and. kkiter == 1 ) THEN
               IF ( PRMS_land_iteration_flag==1 ) THEN
                 retval = intcp()
                 IF ( retval/=0 ) THEN
@@ -699,7 +699,48 @@ C7C2A---FORMULATE THE FINITE DIFFERENCE EQUATIONS.
      2                              IUNIT(55),IGRID)   !cjm (added IUNIT(8))
             IF(IUNIT(66).GT.0 )
      1         CALL GWF2AG7FM(Kkper, Kkstp, Kkiter,IUNIT(63),AGCONVERGE)
-            
+            IF ( Szcheck==ACTIVE .and. kkiter == 1 ) THEN
+              IF ( PRMS_land_iteration_flag==1 ) THEN
+                retval = intcp()
+                IF ( retval/=0 ) THEN
+                  PRINT 9001, 'intcp', retval
+                  RETURN
+                ENDIF
+                retval = snowcomp()
+                IF ( retval/=0 ) THEN
+                  PRINT 9001, 'snowcomp', retval
+                  RETURN
+                ENDIF
+                IF ( Glacier_flag==ACTIVE ) THEN
+                  retval = glacr()
+                  IF ( retval/=0 ) THEN
+                    PRINT 9001, 'glacr_melt', retval
+                    RETURN
+                  ENDIF
+                ENDIF
+              ENDIF
+              IF ( PRMS_land_iteration_flag>0 ) THEN
+                retval = srunoff()
+                IF ( retval/=0 ) THEN
+                  PRINT 9001, 'srunoff', retval
+                  RETURN
+                ENDIF
+              ENDIF
+              IF ( AG_flag==ACTIVE ) THEN
+                retval = soilzone_ag(AFR)
+              ELSE
+                retval = soilzone(AFR)
+              ENDIF
+              IF ( retval/=0 ) THEN
+                PRINT 9001, Soilzone_module, retval
+                RETURN
+              ENDIF
+              retval = gsflow_prms2mf()
+              IF ( retval.NE.0 ) THEN
+                PRINT 9001, 'gsflow_prms2mf', retval
+                RETURN
+              ENDIF
+            END IF
             IF(IUNIT(50).GT.0) THEN
               IF (IUNIT(1).GT.0) THEN
                 CALL GWF2MNW27BCF(KPER,IGRID)
@@ -816,6 +857,9 @@ C
         IF(IUNIT(44).GT.0) CALL LAK2MODSIM(DELTAVOL, LAKEVOL, 
      1                              Diversions, Nsegshold)
       ENDIF
+      IF (Model>=10 .AND. iss==0) THEN
+        IF( IUNIT(66).GT.0 ) CALL AG2MODSIM(Diversions)
+      END IF
       END SUBROUTINE MFNWT_RUN
 C
 C     ************************************************************************
