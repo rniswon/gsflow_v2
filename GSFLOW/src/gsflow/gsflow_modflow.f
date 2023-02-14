313c8ee18f2540722d2108f9c2434094168c3561
rniswon <rniswon@usgs.gov>
2023-02-14 10:22:41 -0800

    changed commit hash

--- GSFLOW/src/gsflow/gsflow_modflow.f
+++ GSFLOW/src/gsflow/gsflow_modflow.f
@@ -44,7 +44,7 @@ C2------WRITE BANNER TO SCREEN AND DEFINE CONSTANTS.
      &        /, 25X, 'HFB, HUF, LAK LPF, MNW1, MNW2, NWT, PCG,',
      &        /, 25X, 'AG, SFR, SIP, UPW, UZF, WEL, SWI, SWT, LMT', /)
 
-        WRITE(*,'(24X,A)') 'Github Commit Hash 0a338c0'
+        WRITE(*,'(24X,A)') 'Github Commit Hash 3c1f7dd'
 
         ! Allocate local module variables
         ALLOCATE ( Mfq2inch_conv(Nhrucell), Mfvol2inch_conv(Nhrucell) )
