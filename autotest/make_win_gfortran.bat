935d6757a73001cae9983007fea414cda2c44fbd
rniswon <rniswon@usgs.gov>
2023-02-23 10:37:36 -0800

    updated path of gfortran on make_win, changed solver values for Trig tests, updated output-test files for these tests

--- autotest/make_win_gfortran.bat
+++ autotest/make_win_gfortran.bat
@@ -1,4 +1,4 @@
-set PATH=%PATH%;C:\Program Files (x86)\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin
+set PATH=%PATH%;C:\Program Files (x86)\mingw64\bin
 gfortran --version
 gcc --version
 C:\Users\rniswon\Anaconda3\envs\py39\python.exe make_gfortran.py -fc gfortran -cc gcc -sd -mc -e ..\GSFLOW\src gsflow.exe
