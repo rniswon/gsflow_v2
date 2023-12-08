6b325b544e5181ac65cff5e1b586914d1c0e2c58
Steve Regan <rsregan@usgs.gov>
2023-12-08 09:47:02 -0700

    update hash and dates

--- GSFLOW/src/gsflow/gsflow_modflow.f
+++ GSFLOW/src/gsflow/gsflow_modflow.f
@@ -44,7 +44,7 @@ C2------WRITE BANNER TO SCREEN AND DEFINE CONSTANTS.
      &        /, 25X, 'HFB, HUF, LAK LPF, MNW1, MNW2, NWT, PCG,',
      &        /, 25X, 'AG, SFR, SIP, UPW, UZF, WEL, SWI, SWT, LMT', /)
 
-        WRITE(*,'(24X,A)') 'Github Commit Hash 8678ee6'
+        WRITE(*,'(24X,A)') 'Github Commit Hash 9c856ec'
 
         ! Allocate local module variables
         ALLOCATE ( Mfq2inch_conv(Nhrucell), Mfvol2inch_conv(Nhrucell) )
