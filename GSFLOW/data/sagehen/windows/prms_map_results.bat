e6b79c1024e6ea775f00febf507ac984ad867b22
Steve Regan <rsregan@usgs.gov>
2024-09-24 13:30:26 -0600

    ran with release exe and hash

--- GSFLOW/data/sagehen/windows/prms_map_results.bat
+++ GSFLOW/data/sagehen/windows/prms_map_results.bat
@@ -1,9 +1,8 @@
 @ECHO OFF
 
 ..\..\..\bin\gsflow .\prms_map_results.control > ..\output\prms\screen_prmsonlyMAP.log
-copy recharge.monthly ..\output\prms\
-copy recharge.total ..\output\prms\
-copy recharge.yearly ..\output\prms\
-del recharge.*
+move /y recharge.monthly ..\output\prms\
+move /y recharge.total ..\output\prms\
+move /y recharge.yearly ..\output\prms\
 ECHO.
 ECHO Run complete. 
