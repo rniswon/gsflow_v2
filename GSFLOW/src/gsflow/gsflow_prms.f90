eb0630436ee17d92235c59933972e50f3d26c4b5
Rich Niswonger <richardniswonger3@gmail.com>
2026-04-27 11:07:37 -0700

    added fix to allow gsflow models without streams

--- GSFLOW/src/gsflow/gsflow_prms.f90
+++ GSFLOW/src/gsflow/gsflow_prms.f90
@@ -21,6 +21,7 @@
       use prms_utils, only: error_stop, numchars, print_module, PRMS_open_output_file, read_error
       USE MF_DLL, ONLY: gsfdecl, MFNWT_RUN, MFNWT_CLEAN, MFNWT_OCBUDGET, MFNWT_INIT
       USE GWFSFRMODULE, ONLY: NSS
+      USE GLOBAL, ONLY: IUNIT
       IMPLICIT NONE
 ! Arguments
       INTEGER, INTENT(IN) :: Process_mode
@@ -247,11 +248,16 @@
             PRINT *, '       NLAKES=', NLAKES_MF, '; Nlake=', Nlake
             ERROR STOP ERROR_modflow
           ENDIF
+          IF (IUNIT(44) == 0) THEN
+            ALLOCATE ( NSS )
+            NSS = 0
+          ENDIF
           IF ( Nsegment/=NSS ) THEN
             PRINT *, 'ERROR, NSS not equal to nsegment'
             PRINT *, '       NSS=', NSS, '; nsegment=', Nsegment
             STOP ERROR_modflow
           ENDIF
+
      !     IF ( irrigation_apply_flag>0 .AND. Ag_package==OFF ) CALL error_stop( &
      !&         'irrigation_apply_flag > 0 without AG Package active', ERROR_control)
           IF ( irrigation_apply_flag>0 .AND. Ag_package==OFF ) THEN
